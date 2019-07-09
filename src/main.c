941a2cba0b99a209e50a4e451ec279128902a042
Pablo Tesone <tesonep@gmail.com>
2019-07-09 08:26:34 +0200

    Adding experiment to run the VM in other thread

--- src/main.c
+++ src/main.c
@@ -5,7 +5,22 @@
 #include <pharoClient.h>
 #include <pharo.h>
 
+
+int loadAndExecuteVM(VM_PARAMETERS* parameters){
+	if(!initPharoVM(parameters->imageFile, parameters->vmParams, parameters->vmParamsCount, parameters->imageParams, parameters->imageParamsCount)){
+		logError("Error opening image file: %s\n", parameters->imageFile);
+		exit(-1);
+	}
+	runInterpreter();
+}
+
+int runThread(void* parameters){
+	loadAndExecuteVM((VM_PARAMETERS*) parameters);
+}
+
+
 int main(int argc, char* argv[]){
+	pthread_t thread_id;
 
 	VM_PARAMETERS parameters;
 
@@ -13,11 +28,29 @@ int main(int argc, char* argv[]){
 
 	logInfo("Opening Image: %s\n", parameters.imageFile);
 
-	if(!initPharoVM(parameters.imageFile, parameters.vmParams, parameters.vmParamsCount, parameters.imageParams, parameters.imageParamsCount)){
-		logError("Error opening image file: %s\n", parameters.imageFile);
-		return -1;
+	//This initialization is required because it makes awful, awful, awful code to calculate
+	//the location of the machine code.
+	//Luckily, it can be cached.
+	osCogStackPageHeadroom();
+
+	pthread_attr_t tattr;
+
+	pthread_attr_init(&tattr);
+
+	size_t size;
+	pthread_attr_getstacksize(&tattr, &size);
+
+	printf("%ld\n", size);
+
+    if(pthread_attr_setstacksize(&tattr, size*4)){
+		perror("Thread attr");
+    }
+
+	if(pthread_create(&thread_id, &tattr, runThread, &parameters)){
+		perror("Thread creation");
 	}
-	runInterpreter();
+
+	pthread_join(thread_id, NULL);
 }
 
 void printVersion(){

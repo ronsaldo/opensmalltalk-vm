3ddd0d3adbfd0b81eba51cc9c180a48188ffd4fa
Ronie Salgado <roniesalg@gmail.com>
2019-12-23 22:47:04 -0300

    Fix the path of some includes.

--- src/win32MainGToolkit.c
+++ src/win32MainGToolkit.c
@@ -1,6 +1,6 @@
-#include "pharoClient.h"
-#include "fileDialog.h"
-#include "pathUtilities.h"
+#include "pharovm/pharoClient.h"
+#include "pharovm/fileDialog.h"
+#include "pharovm/pathUtilities.h"
 
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
